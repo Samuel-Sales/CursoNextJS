caf7cea1c70fe7eb9d9f9078a0e286587834dedd
Samuel-Sales <cacabrbf@hotmail.com>
2024-07-19 22:29:03 -0300

    Aula 6 completa

--- reactjs/primeiroapp/src/App.jsx
+++ reactjs/primeiroapp/src/App.jsx
@@ -1,7 +1,13 @@
+import Nome from "./components/Nome";
+
 function App() {
   return (
-    <h1>Bem vindo</h1>
+    <div>
+      <h1>Componente App</h1>
+      <Nome aluno="Samuel" idade={25} /> <br />
+      <Nome aluno="Matheus" idade={30} />
+    </div>
   )
 }
 
-export default App
+export default App;
