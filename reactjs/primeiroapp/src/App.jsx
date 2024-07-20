15b255856028c4c031c22c7ad45fd7e881b152e3
Samuel-Sales <cacabrbf@hotmail.com>
2024-07-20 09:16:40 -0300

    Aula 8 completa

--- reactjs/primeiroapp/src/App.jsx
+++ reactjs/primeiroapp/src/App.jsx
@@ -1,21 +1,46 @@
 import { useState } from "react";
-import Nome from "./components/Nome";
+
 
 
 function App() {
-  const [aluno, setAluno] = useState('Desenvolvedor vagabundo');
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
 
-  function handleChangeName(nome) {
-    setAluno(nome);
+  const [user, setUser] = useState({});
+
+  function handleRegister(e) {
+    e.preventDefault();
+    alert('Usuario registrado com sucesso')
+
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email
+    })
   }
 
   return (
     <div>
-      <h1>Componente App</h1> 
-      <h2>Ola: {aluno}</h2>
-      <button onClick={() => handleChangeName('Samuel W')}>
-        Mudar nome
-      </button>
+      <h1>Cadastrando Usuario</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome: </label><br />
+        <input type="text" value={nome} placeholder="Digite seu nome" onChange={ (e) => setNome(e.target.value) } />
+        <label>Email: </label>
+        <input type="text" value={email} placeholder="Digite seu email" onChange={ (e) => setEmail(e.target.value) } /> <br />
+        <label>Idade: </label><br />
+        <input type="text" value={idade} placeholder="Digite sua idade" onChange={ (e) => setIdade(e.target.value) }/> <br />
+
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br />
+      <div>
+        <span>Bem vindo: {user.nome}</span> <br />
+        <span>Idade: {user.idade}</span> <br />
+        <span>Email: {user.email}</span>
+      </div>
     </div>
   )
 }
