51f4eac4f4b3b4c165c06bed7f4104a2aa002154
Samuel-Sales <cacabrbf@hotmail.com>
2024-07-20 08:58:08 -0300

    Aula

--- reactjs/primeiroapp/src/App.jsx
+++ reactjs/primeiroapp/src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Nome from "./components/Nome";
 
+
 function App() {
+  const [aluno, setAluno] = useState('Desenvolvedor vagabundo');
+
+  function handleChangeName(nome) {
+    setAluno(nome);
+  }
+
   return (
     <div>
-      <h1>Componente App</h1>
-      <Nome aluno="Samuel" idade={25} /> <br />
-      <Nome aluno="Matheus" idade={30} />
+      <h1>Componente App</h1> 
+      <h2>Ola: {aluno}</h2>
+      <button onClick={() => handleChangeName('Samuel W')}>
+        Mudar nome
+      </button>
     </div>
   )
 }
