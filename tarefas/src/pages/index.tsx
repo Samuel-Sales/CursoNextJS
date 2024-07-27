798788222c87438d1d84aad2cbc175292638f8db
Samuel-Sales <cacabrbf@hotmail.com>
2024-07-26 23:20:43 -0300

    Criando projeto

---
+++ tarefas/src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
+import heroImg from '../../public/assets/hero.png'
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefas+ | Organize suas tarefas de forma facil</title>
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image className={styles.hero} alt='Logo Tarefas+' src={heroImg} priority />
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e terefas
+        </h1>
+      </main>
+    </div>
+  )
+}
